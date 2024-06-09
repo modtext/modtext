9f2fc37e2d9dcf8f0c1e588f115a06308d9d46ae
A K <alex.kesling@gmail.com>
2024-06-09 12:02:46 -0400

    Stub out basic example of building a Document in modtext main.rs

--- modtext/src/main.rs
+++ modtext/src/main.rs
@@ -1,3 +1,29 @@
+use schema::mod_text::{Block, BlockArgs, Document, DocumentArgs};
+
 fn main() {
-    println!("Hello, world!");
+    let mut builder = flatbuffers::FlatBufferBuilder::with_capacity(1024);
+
+    let symbol_strings = vec![
+        builder.create_string("Hello"),
+        builder.create_string("%Text"),
+        builder.create_string("Document")
+    ];
+    let symbols = builder.create_vector(&symbol_strings);
+
+    let root = Block::create(&mut builder, &BlockArgs {
+        processor: 0,
+        parameters: None,
+        body: None,
+    });
+    let doc = Document::create(&mut builder, &DocumentArgs {
+        processors: None,
+        symbols: Some(symbols),
+        root: Some(root),
+    });
+
+    builder.finish(doc, None);
+    let buf = builder.finished_data();
+
+    let doc = flatbuffers::root::<Document>(buf).unwrap();
+    println!("Hello, flatbuffers!: {:?}", doc);
 }
