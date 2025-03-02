d72ea6c1ddb44463de8b968e6a2a0f08c018672f
A K <alex.kesling@gmail.com>
2025-03-01 21:07:33 -0500

    feat: start playing around with more ergonomic builder helpers

--- modtext/src/main.rs
+++ modtext/src/main.rs
@@ -3,6 +3,33 @@ use schema::mod_text::{
     FragmentArgs, FragmentType, Processor, ProcessorArgs,
 };
 
+fn processors_array<
+    'bldr: 'args,
+    'args: 'mut_bldr,
+    'mut_bldr,
+    A: flatbuffers::Allocator + 'bldr,
+>(
+    items: &[flatbuffers::WIPOffset<Processor<'bldr>>],
+    builder: &'mut_bldr mut flatbuffers::FlatBufferBuilder<'bldr, A>,
+) -> flatbuffers::WIPOffset<
+    flatbuffers::Vector<'bldr, flatbuffers::ForwardsUOffset<schema::mod_text::Processor<'bldr>>>,
+> {
+    builder.create_vector(items)
+}
+
+fn processor<'bldr: 'args, 'args: 'mut_bldr, 'mut_bldr, A: flatbuffers::Allocator + 'bldr>(
+    name: &str,
+    builder: &'mut_bldr mut flatbuffers::FlatBufferBuilder<'bldr, A>,
+) -> flatbuffers::WIPOffset<Processor<'bldr>> {
+    let proc_name = builder.create_string(name);
+    Processor::create(
+        builder,
+        &ProcessorArgs {
+            name: Some(proc_name),
+        },
+    )
+}
+
 fn main() {
     let mut builder = flatbuffers::FlatBufferBuilder::with_capacity(1024);
 
@@ -13,14 +40,13 @@ fn main() {
     ];
     let symbols = builder.create_vector(&symbol_strings);
 
-    let proc_0_name = builder.create_string("format");
-    let proc_0 = Processor::create(
+    let processors = processors_array(
+        &[
+            processor("format", &mut builder),
+            processor("style", &mut builder),
+        ],
         &mut builder,
-        &ProcessorArgs {
-            name: Some(proc_0_name),
-        },
     );
-    let processors = builder.create_vector(&vec![proc_0]);
     // TODO(akesling): These should _really_ be represented by sub-blocks of processor type
     // "symbol" which take an index paremeter.
     let root_template = builder.create_string("%symbol{0} %symbol{1} %symbol{2}!");
@@ -37,7 +63,7 @@ fn main() {
             value_type: FragmentType::DocumentSpan,
         },
     );
-    let root_fragments = builder.create_vector(&vec![root_body]);
+    let root_fragments = builder.create_vector(&[root_body]);
     let root = Block::create(
         &mut builder,
         &BlockArgs {
@@ -59,5 +85,5 @@ fn main() {
     let buf = builder.finished_data();
 
     let doc = flatbuffers::root::<Document>(buf).unwrap();
-    println!("Hello, flatbuffers!: {:?}", doc);
+    println!("Hello, flatbuffers!: {:#?}", doc);
 }
